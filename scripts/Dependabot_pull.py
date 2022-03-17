c411440407add7ba492fc66a2c20bc39fe209a5d
jaibalajiCSW <100219826+jaibalajiCSW@users.noreply.github.com>
2022-03-17 13:00:59 +0530

    Update Dependabot_pull.py

--- scripts/Dependabot_pull.py
+++ scripts/Dependabot_pull.py
@@ -1,14 +1,12 @@
-import requests
+mport requests
 import json
 import csv
 import os
 
-Owner = "Akashbalaji1410"  
-Repo = "Elanco"
-repo_url = f'https://github.com/'+Owner+'/'+Repo+'/' # Enter URL of desired repo and owner of the repo.
-headers = {"Authorization": "Bearer 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"} # Enter Github API key
+repo_url = f'https://github.com/' + os.getenv('OWNER') + '/' + os.getenv('REPO') + '/'# Enter URL of desired repo and owner of the repo. (Secrets)
+# headers = {"Authorization":  os.getenv('AUTH_TOKEN1')} # Enter Github API key of the target Repo (Secrets)
+headers = {"Authorization":  "Bearer "+os.getenv('AUTH_TOKEN1')}
 severity_mapping = {'CRITICAL': 10, 'HIGH': 8.9, 'MODERATE': 6.9, 'LOW': 3.9}
-
 def run_query(query):
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
     if request.status_code == 200:
@@ -18,15 +16,18 @@ def run_query(query):
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
 
+
+
+
 # The GraphQL query ( Enter login : OWNER NAME and name : REPOSITORY NAME)
 # Add 
 query = """{ 
     viewer 
     {
-    organization(login: """+"\""+Owner+"\"" +""") { 
+   organization(login: """+"\""+os.getenv('OWNER')+"\"" +""") { 
       id
     }
-    repository(name: """+"\""+Repo+"\"" +""") {
+    repository(name: """+"\""+os.getenv('REPO')+"\"" +""") {
       id
       name
       createdAt
@@ -71,9 +72,8 @@ with open('Dependabot_Alerts.json', 'w') as out_file: # Writing JSON data into a
 with open("Dependabot_Alerts.json") as json_format_file: # Parsing the JSON data. 
   j = json.load(json_format_file)
 
-
 #Writing JSON data in CSV
-csv = open("Dependabot.csv","w")
+csv = open(r".\files_to_process\Dependabot.csv","w")
 csvdata = "Name,Pluginid,Created At,Description,Summary,Reference Type,Solution,BaseUrl,Severity,Package Name,Application URL,File Path\n"
 csvdata += j['data']['viewer']['repository']['name'] + ","
 for i in j['data']['viewer']['repository']['vulnerabilityAlerts']['nodes']:
@@ -91,5 +91,3 @@ for i in j['data']['viewer']['repository']['vulnerabilityAlerts']['nodes']:
 csv.write(csvdata)
 csv.close()
 print(csvdata)
-
-
