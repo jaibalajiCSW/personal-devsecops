f2b0433c0e11fab01456f0201d78551d57144b95
jaibalajiCSW <100219826+jaibalajiCSW@users.noreply.github.com>
2022-03-17 12:57:52 +0530

    Add files via upload

---
+++ scripts/Dependabot_pull.py
@@ -0,0 +1,95 @@
+import requests
+import json
+import csv
+import os
+
+Owner = "Akashbalaji1410"  
+Repo = "Elanco"
+repo_url = f'https://github.com/'+Owner+'/'+Repo+'/' # Enter URL of desired repo and owner of the repo.
+headers = {"Authorization": "Bearer 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"} # Enter Github API key
+severity_mapping = {'CRITICAL': 10, 'HIGH': 8.9, 'MODERATE': 6.9, 'LOW': 3.9}
+
+def run_query(query):
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+        
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+
+# The GraphQL query ( Enter login : OWNER NAME and name : REPOSITORY NAME)
+# Add 
+query = """{ 
+    viewer 
+    {
+    organization(login: """+"\""+Owner+"\"" +""") { 
+      id
+    }
+    repository(name: """+"\""+Repo+"\"" +""") {
+      id
+      name
+      createdAt
+      vulnerabilityAlerts(first: 100) {
+        nodes {
+          id
+          securityVulnerability {
+            package {
+              ecosystem
+              name
+            }
+            severity
+            advisory 
+            {
+              cvss {
+                score
+              }
+              description
+              ghsaId
+              summary
+              origin
+              publishedAt
+              references {
+                url
+              }
+              description
+              updatedAt
+            }
+          }
+          vulnerableManifestFilename
+          vulnerableManifestPath
+        }
+      }
+    }
+  }
+}"""
+result = run_query(query) # Execute the query
+
+with open('Dependabot_Alerts.json', 'w') as out_file: # Writing JSON data into a file.
+  out_file.write(json.dumps(result))
+
+with open("Dependabot_Alerts.json") as json_format_file: # Parsing the JSON data. 
+  j = json.load(json_format_file)
+
+
+#Writing JSON data in CSV
+csv = open("Dependabot.csv","w")
+csvdata = "Name,Pluginid,Created At,Description,Summary,Reference Type,Solution,BaseUrl,Severity,Package Name,Application URL,File Path\n"
+csvdata += j['data']['viewer']['repository']['name'] + ","
+for i in j['data']['viewer']['repository']['vulnerabilityAlerts']['nodes']:
+  csvdata += i['id'] + "," #REQUIRED
+  csvdata += i['securityVulnerability']['advisory']['publishedAt'][0:10] + "," 
+  csvdata += "\"" + i['securityVulnerability']['advisory']['description']+ "\"" + "," #REQUIRED
+  csvdata += i['securityVulnerability']['advisory']['summary'] + ' in ' + i['vulnerableManifestPath'] + "," #REQUIRED
+  csvdata += i['securityVulnerability']['package']['ecosystem'] + "," #REQUIRED
+  csvdata += 'Listed as Recommendation under Description' + "," #REQUIRED
+  csvdata += 'https://github.com' + "," #REQUIRED
+  csvdata += str(severity_mapping.get(i['securityVulnerability']['severity'])) + "," #REQUIRED
+  csvdata += i['securityVulnerability']['package']['name'] + "," #REQUIRED
+  csvdata += repo_url + "," #REQUIRED
+  csvdata += repo_url + i['vulnerableManifestPath'] + "," #REQUIRED
+csv.write(csvdata)
+csv.close()
+# print(csvdata)
+
+
---
+++ scripts/Upload_to_Platforn_depend.py
@@ -0,0 +1,265 @@
+import json
+from datetime import datetime
+import os
+import sys
+import toml
+import requests
+from urllib3 import Retry
+from requests.adapters import HTTPAdapter
+from zipfile import ZipFile
+
+
+def read_config_file(filename):
+
+    try:
+        data = toml.loads(open(filename).read())
+        return data
+    except (Exception, FileNotFoundError, toml.TomlDecodeError) as ex:
+        print("Error reading configuration file.")
+        print(ex)
+        print()
+        input("Please press ENTER to close.")
+        exit(1)
+
+def process_config(config):
+  try:
+    platform_url = config['platform_url']
+    api_key = config['api_key']
+    #json_filename = config['json_filename']
+    client_id = config['client_id']
+    auth_token = "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"
+    git_user = config['git_user']
+    repo_name = config['repo_name']
+    zipfile_name = config['zipfile_name']
+    network_id = config['network_id']
+  except:
+    print("Error accessing/using data from the config file.")
+    print("The config file must contain the following values")
+    print("platform_url, api_key, client_id, network_id, git_user, repo_name, auth_token, artifact_id, zipfile_name, json_filename")
+  return platform_url, api_key, client_id, network_id, git_user, repo_name, auth_token, zipfile_name
+
+
+def __requests_retry_session(max_retries=5, backoff_factor=0.5,
+                             status_forcelist=(429, 502, 503)):
+
+    session = requests.Session()
+    retry = Retry(total=max_retries,
+                  read=max_retries,
+                  connect=max_retries,
+                  backoff_factor=backoff_factor,
+                  status_forcelist=status_forcelist)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('https://', adapter)
+    return session
+
+
+#CREATING A NEW ASSESSMENT 
+#THE ASSESSMENT NAME IS OF THE FORMAT APPTHREAT_<DATE>_<TIME>
+def create_assessment(platform_url,api_key,client_id):
+
+  startDate = str(datetime.now().strftime('%Y-%m-%d'))
+  startTime = str(datetime.now().strftime('%H:%M'))
+  assess_name = "Dependabot_"+ startDate + "_" + startTime
+  url = "{}//api/v1/client/{}/assessment".format(
+      platform_url, client_id)
+
+  header = {
+      "content-type": "application/json",
+      "x-api-key": api_key}
+ 
+  body = {
+      "name": assess_name,
+      "startDate": startDate,
+      "notes": "",
+      "startTime": startTime
+  }
+
+  try:
+      raw_response = __requests_retry_session().post(
+          url, headers=header, data=json.dumps(body))
+  except TimeoutError as ex:
+      print(ex)
+  response = json.loads(raw_response.text)
+  if raw_response and raw_response.status_code == 201:
+      response = json.loads(raw_response.text)
+      print("Assessment successfully created ")
+      print("[+] Assessment Name : ",assess_name)
+      return response['id']
+  else:
+      print(response)
+      print(response.content)
+      print('Error while creating the assessment')
+      sys.exit(0)
+
+
+# GETTING THE UPLOAD ID  
+def get_upload_id(platform_url, api_key, client_id, assessment_id, network_id):
+
+  url = "{}//api/v1/client/{}/upload".format(
+      platform_url, client_id)
+
+  header = {
+      "content-type": "application/json",
+      "x-api-key": api_key}
+
+  body = {
+      "assessmentId": assessment_id,
+      "name":str(datetime.now().strftime('%Y-%m-%d')),
+      "networkId":network_id
+  }
+
+  try:
+      raw_response = __requests_retry_session().post(
+          url, headers=header, data=json.dumps(body))
+  except TimeoutError as ex:
+      print(ex)
+
+  if raw_response and raw_response.status_code == 201:
+      response = json.loads(raw_response.text)
+      return response['id']
+  else:
+      print('Error while getting upload id...')
+      print("Exitting...")
+      sys.exit(0)
+
+
+#Upload the file using the upload_id 
+def upload_file(upload_id,platform_url,client_id,api_key,csv_filename):
+
+  url = "{}//api/v1/client/{}/upload/{}/file".format(
+      platform_url, client_id, upload_id)
+
+  header = {
+      "x-api-key": api_key,
+      "Content-Disposition": "form-data",
+      "filename": csv_filename,
+       }
+
+  scanFile={'scanFile': open(csv_filename,'rb')}
+
+  try:
+      raw_response = __requests_retry_session().post(
+          url, files=scanFile, headers=header)
+  except TimeoutError as ex:
+      print(ex)
+
+  if raw_response and raw_response.status_code == 201:
+      #response = json.loads(raw_response.text) 
+      return
+  else:
+      print("Error while uploading the file ")
+      print("Exitting...")
+      sys.exit(0)
+
+
+  
+#PARSE THE UPLOADED FILE
+def start_parsing(upload_id, platform_url, client_id, api_key ):
+
+  url = "{}//api/v1/client/{}/upload/{}/start".format(
+      platform_url, client_id,upload_id)
+
+  header = {
+      "content-type": "application/json",
+      "x-api-key": api_key
+      }
+
+  body = {
+      "autoUrba": False 
+  }
+
+  try:
+      raw_response = __requests_retry_session().post(
+          url, headers=header, data=json.dumps(body))
+  except TimeoutError as ex:
+      print(ex)
+
+  if raw_response and raw_response.status_code == 200:
+      #response = json.loads(raw_response.text)    
+      print("Successfully started parsing the uploaded file :)")
+  else:
+      print("Couldnt start parsing the file")
+      print("Exitting...")
+      sys.exit(0)
+
+  
+def extract_zip(git_user,repo_name,artifact_id,auth_token,zipfile_name):
+
+        print("\n\nGathered Info :: ")
+        print("[+] Github Username : ", git_user)
+        print("[+] Repository Name : ", repo_name)
+        print("[+] Artifact ID : ", artifact_id)
+        print('Fetching the artifact from the desired Github Repository...')
+        url = "https://api.github.com/repos/{0}/{1}/actions/artifacts/{2}/zip".format(git_user,repo_name,artifact_id)
+        payload={}
+        headers = {
+          'Authorization': 'Bearer {0}'.format(auth_token)
+        }
+        response = requests.get(url, headers=headers, data=payload, timeout=5)
+        file_name = zipfile_name
+        file = open(file_name, "wb")
+        file.write(response.content)
+        file.close()
+        try:
+          with ZipFile(file_name, 'r') as zip:
+            print('\nExtracting the file now...')
+            zip.extract(member="depend.csv", path=".")
+        except: 
+          print("Error while extracting the file...")
+          print("Exitting...")
+          sys.exit(0)
+
+def get_artifact_id(git_user, repo_name,auth_token):
+  url = "https://api.github.com/repos/{}/{}/actions/artifacts".format(git_user,repo_name)
+  payload={}
+  header = {
+  'Authorization': 'Bearer {0}'.format(auth_token)
+  }
+  try:
+      raw_response = __requests_retry_session().get(
+          url, headers=header, data=json.dumps(payload))
+  except TimeoutError as ex:
+      print("Error while getting the artifact id. ")
+      print(ex)
+
+  if raw_response and raw_response.status_code == 200:
+      response = json.loads(raw_response.text)  
+      artifact_id = response['artifacts'][0]['id']
+      return artifact_id
+
+
+#MAIN
+def main():
+
+  #READING THE CONFIG FILE 
+  conf_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'conf', 'config.toml')
+  config = read_config_file(conf_file)
+  platform_url, api_key, client_id,network_id, git_user, repo_name, auth_token, zipfile_name = process_config(config)
+  #CHECKING FOR MISSING VARIABLES
+  if (git_user == "" or repo_name == "" or auth_token == "" or zipfile_name == "" ):
+    print("Missing one or more of the following values ")
+    print("[+] Github Username \n[+] Repository Name\n[+] Github Access Token \n[+] ZipFile Name \n[+] JsonFile Name ")
+    sys.exit(0)
+
+  #DOWNLOADING AND EXTRACTING THE ZIP FILE OF ARTIFACT 
+  artifact_id = get_artifact_id(git_user, repo_name, auth_token)
+  extract_zip(git_user,repo_name,artifact_id,auth_token,zipfile_name)
+
+  #CONVERTING THE JSON FILE TO CSV
+  #csv_filename = jsontocsv(json_filename)
+  File_name="depend.csv"
+  #BUCKLE UP...
+  assessment_id = create_assessment(platform_url, api_key, client_id)
+  upload_id = get_upload_id(platform_url, api_key, client_id, assessment_id, network_id)
+  upload_file(upload_id,platform_url,client_id,api_key,File_name)
+  start_parsing(upload_id, platform_url, client_id, api_key)
+
+#  Execute the script
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        print("KeyboardInterrupt detected.  Exiting...")
+        print()
+        sys.exit(0)
